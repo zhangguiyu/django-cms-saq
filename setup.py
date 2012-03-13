8269ab32c12a8241cb67b8a304248190c2f1c54b
Olly Smith <oliver.smith@maplecroft.com>
2012-03-13 15:46:37 +0000

    Setuptools stuff.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import cms_saq
+
+version = cms_saq.__version__
+
+setup(
+    name='django-cms-saq',
+    version=version,
+    description='Self-assessment questionnaires with Django CMS.',
+    author='Olly Smith',
+    author_email='oliver.smith@maplecroft.com',
+    url='http://git.maplecroft.com/',
+    packages=['cms_saq'],
+    license='Maplecroft',
+    include_package_data = True,
+)
